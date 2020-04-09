c525ec8de9250ae7951b7d82ce8d03941cf044a5
Delta145 <sawin.georgij@yandex.ru>
2020-04-09 17:40:50 +0300

    optimized code in Langrage Polinomial class

--- src/main/scala/io/CommandHandler.scala
+++ src/main/scala/io/CommandHandler.scala
@@ -8,8 +8,9 @@ class CommandHandler {
     var sampleN: Option[Int] = None
     while (sampleN.isEmpty || sampleN.get < 1 || sampleN.get > 4) {
       println("Выберите один из предложенных наборов данных\n" +
-        "1) нелинейное уравнение a*x^3 - b*x^2 - c*x + d методом хорд\n" +
-        "2) нелинейное уравнение a*x^3 - b*x^2 - c*x + d методом секущих\n" +
+        "1) [0, 2/3*PI, PI, 2*PI]\n" +
+        "   [1, -0.5, -1, 1]\n" +
+        "2) \n" +
         "3) систему нелинейных уравнений \n" +
         "\ty = a*sin(x) + b\n" +
         "\ty = c*e^x + d")
--- src/main/scala/method/LagrangePolynomial.scala
+++ src/main/scala/method/LagrangePolynomial.scala
@@ -1,42 +1,32 @@
 package method
 
-import scala.collection.mutable.ListBuffer
-
 class LagrangePolynomial(val X: List[Double], val Y: List[Double]) {
   def getFunction(): Double => Double = {
-    val function = ListBuffer.empty[Double => Double]
-
-    for((v,i) <- Y.view.zipWithIndex) {
-      function.addOne((x: Double) => v/getDenominator(i)*getNumerator(i)(x))
-    }
-
-    (x: Double) => {
+    x => {
       var sum = 0.0
-      function.foreach((f: Double => Double) => sum += f(x))
+      for ((y,i) <- Y.view.zipWithIndex) {
+        sum += y/getDenominator(i)*getNumerator(i)(x)
+      }
       sum
     }
   }
 
   def getNumerator(k: Int): Double => Double = {
-    val numerator = ListBuffer.empty[Double => Double]
-
-    for ((v, i) <- X.view.zipWithIndex) {
-      if (i != k)
-        numerator.addOne((x: Double) => x - v)
-    }
-
-    (x: Double) => {
+    x => {
       var sum = 1.0
-      numerator.foreach((f: Double => Double) => sum *= f(x))
+      for ((xi, i) <- X.view.zipWithIndex) {
+        if (i != k)
+          sum *= x - xi
+      }
       sum
     }
   }
 
   def getDenominator(k: Int): Double = {
     var ans = 1.0
-    for((v,i) <- X.view.zipWithIndex) {
+    for((xi,i) <- X.view.zipWithIndex) {
       if (k != i)
-        ans *= X(k) - v
+        ans *= X(k) - xi
     }
     ans
   }
